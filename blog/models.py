d4a22281420a3bd04ea91786a4be4af2d1941a94
José Carlos <josecarcatarubio6@gmail.com>
2024-11-21 17:48:16 +0100

    Fin diapositiva

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=60)
+    cuerpo = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.titulo
+    
